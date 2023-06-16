e35b1cf32fa34a5a4946d85b7e8cae830998c68a
rogerthatdev <roger.that.dev@gmail.com>
2023-06-16 10:01:33 -0700

    boilerplate tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+# Pubsub subscription to cloud-builds topic
+
+# Event handling Cloud function
+
+# Corresponding trigger Cloud Function
+
+# Service account that will execute event handler cloud function 
+
+# Service account that will file issue to Github
+
